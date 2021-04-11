eb06db8d1106816b3d2af0109ee5881a2b7a9756
Prateek Goel <goelprat@amazon.com>
2021-04-10 20:26:28 -0700

    Auto generated code

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,15 +2,14 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:agora_rtc_engine/agora_rtc_engine_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  AgoraRtcEngineWeb.registerWith(registry.registrarFor(AgoraRtcEngineWeb));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  AgoraRtcEngineWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
